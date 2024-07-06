250227ec8411213ae0ebc5577cb81afc5c52beda
Naveen Pandava <naveenpandava@gmail.com>
2024-07-06 07:57:28 +0530

    added security group code

---
+++ launch_template.tf
@@ -0,0 +1,47 @@
+resource "aws_iam_role" "ec2_role" {
+  name = "ec2_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+
+}
+
+
+resource "aws_iam_instance_profile" "ec_inst_profile" {
+  name = "ec2_instance_profile"
+  role = "${aws_iam_role.ec2_role.name}"
+}
+
+
+resource "aws_iam_role_policy" "policy1" {
+  name = "policy1"
+  role = "${aws_iam_role.ec2_role.id}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
